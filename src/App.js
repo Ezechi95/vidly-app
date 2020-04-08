a1e2cf4a9d6c534dfd5fe7dbb17a311eb23452e3
Ezechi Njoku <ezechi_n_njoku@homedepot.com>
2020-04-08 17:48:14 -0400

    Completed routing

--- src/App.js
+++ src/App.js
@@ -2,22 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
 import { getMovies, deleteMovie } from "./services/fakeMovieService";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
+import MovieForm from "./components/movieForm";
 
 class App extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
   };
 
-  handleDelete = movieId => {
+  handleDelete = (movieId) => {
     console.log("Deleted: ", movieId);
     this.setState(deleteMovie(movieId));
   };
 
   render() {
     return (
-      <main className="container m-2">
-        <Movies getMovies={this.state.movies} onDelete={this.handleDelete} />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container m-2">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={(props) => (
+                <Movies
+                  getMovies={this.state.movies}
+                  onDelete={this.handleDelete}
+                  {...props}
+                />
+              )}
+            />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
